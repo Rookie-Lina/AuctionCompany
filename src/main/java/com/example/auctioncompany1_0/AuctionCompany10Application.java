20791dbd8eb8689c5c0c867005a925eb89495a9a
Zhangyiming <183511749@qq.com>
2022-07-11 17:44:53 +0800

    创建项目

---
+++ src/main/java/com/example/auctioncompany1_0/AuctionCompany10Application.java
@@ -0,0 +1,13 @@
+package com.example.auctioncompany1_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionCompany10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionCompany10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auctioncompany1_0/AuctionCompany10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auctioncompany1_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionCompany10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
