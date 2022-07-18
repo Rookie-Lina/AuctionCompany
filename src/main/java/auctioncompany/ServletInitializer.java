e893588a8db0c1d7e0e24708f77345873427837b
Zhangyiming <183511749@qq.com>
2022-07-18 15:35:15 +0800

    创建项目

--- src/main/java/auctioncompany/ServletInitializer.java
+++ src/main/java/auctioncompany/ServletInitializer.java
@@ -1,10 +1,6 @@
-<<<<<<<< HEAD:src/main/java/auctioncompany/ServletInitializer.java
 package auctioncompany;
-========
-package com.auctioncompany;
->>>>>>>> fa73735 (创建项目):src/main/java/com/auctioncompany/ServletInitializer.java
 
-import com.auctioncompany.AuctionCompanyApplication;
+import auctioncompany.AuctionCompanyApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
