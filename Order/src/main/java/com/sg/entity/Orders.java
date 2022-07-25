3bc62c71df39fec7bef43899d1ce1ebdb0c0de45
Rookie-lin <lin15553449273@163.com>
2022-07-25 11:10:09 +0800

    fix #62077858 修改Order表结构

--- Order/src/main/java/com/sg/entity/Orders.java
+++ Order/src/main/java/com/sg/entity/Orders.java
@@ -17,15 +17,12 @@ public class Orders {
     private int id; // 自增ID
     private String orderNo; // 订单号
     private int userId; // 用户ID
-    private double goodsMoney; // 商品总金额
+    private int goodsId; // 商品ID
+    private int addressId; // 收货地址ID
     private int deliverType; // 收货方式
-    private double realTotalMoney; // 实际订单总金额
     private int orderStatus; // 订单状态
     private int payFrom; // 支付来源
     private int isPay; // 是否支付
-    private String userName; // 收货人名称
-    private String userAddress; // 收件人地址
-    private String userPhone; // 收件人手机号码
     private String orderRemarks; // 订单备注
     private Date createTime; // 下单时间
 
