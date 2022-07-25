48a767f5124a3509454807dec20f89bfb7d7f4ec
Rookie-lin <lin15553449273@163.com>
2022-07-25 11:06:40 +0800

    fix #62077858 新增地址表结构

---
+++ User/src/main/java/com/sg/entity/UserAddress.java
@@ -0,0 +1,19 @@
+package com.sg.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * @Description
+ * @auther Rookie_lin
+ * @create 2022-07-25 11:03
+ */
+@Data
+@TableName("users_address")
+public class UserAddress {
+    private int id;
+    private int userId;
+    private String address;
+    private String phone;
+    private String name;
+}
