444ac60fdc936ee1ab9222fb854f72f9344619d5
Rookie-lin <lin15553449273@163.com>
2022-08-05 14:36:25 +0800

    新增用户地址查询

---
+++ Order/src/main/java/com/sg/controller/UserAddressController.java
@@ -0,0 +1,19 @@
+package com.sg.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+/**
+ * @Description
+ * @auther Rookie_lin
+ * @create 2022-08-03 11:26
+ */
+@RestController
+@RequestMapping("/address")
+public class UserAddressController {
+
+
+}
---
+++ Order/src/main/java/com/sg/service/UserAddressService.java
@@ -0,0 +1,13 @@
+package com.sg.service;
+
+import com.sg.entity.UserAddress;
+
+/**
+ * @Description
+ * @auther Rookie_lin
+ * @create 2022-08-03 11:27
+ */
+public interface UserAddressService {
+    UserAddress selectAddressById(int id);
+
+}
